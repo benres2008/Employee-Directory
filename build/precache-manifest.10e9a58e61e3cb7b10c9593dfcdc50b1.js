166f0f3a0efe502ccbfe22e552fda5209f3ceb3a
rez <benjaminresnicoff@icloud.com>
2020-12-03 10:43:07 -0600

    made final edits to CSS

---
+++ build/precache-manifest.10e9a58e61e3cb7b10c9593dfcdc50b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ef7beb92ff67a10c03d19d6859fb53",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "83683b062bfd1436a98b",
+    "url": "/employee-directory/static/css/main.f37213f7.chunk.css"
+  },
+  {
+    "revision": "3ddb980526134067f7d0",
+    "url": "/employee-directory/static/js/2.d6d2d680.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/employee-directory/static/js/2.d6d2d680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83683b062bfd1436a98b",
+    "url": "/employee-directory/static/js/main.7f06558b.chunk.js"
+  },
+  {
+    "revision": "b6aadb01db8e349f9523",
+    "url": "/employee-directory/static/js/runtime-main.e6b3c303.js"
+  }
+]);
